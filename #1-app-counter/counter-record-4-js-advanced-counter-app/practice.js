f14ba6f1221468085117a527d504cee48449f5f5
MOSTAFAMAHFOUZ <emostafamahfouz@gmail.com>
2023-05-04 11:46:41 +0300

    advanced counter app

--- #1-app-counter/counter-record-4-js-advanced-counter-app/practice.js
+++ #1-app-counter/counter-record-4-js-advanced-counter-app/practice.js
@@ -1,24 +1,19 @@
-const btns = document.querySelectorAll(".btn");
-const colorPallet = ["red","green","blue","orange","yellow"];
+// const btns = document.querySelectorAll(".btn");
+// const colorPallet = ["red","green","blue","orange","yellow"];
 
-// console.log(btns[0]);
+// // console.log(btns[0]);
 
-btns.forEach(function(item){
-    item.addEventListener("click",addStyleAction)
-})
+// btns.forEach(function(item){
+//     item.addEventListener("click",addStyleAction)
+// })
 
-
-function addStyleAction(){
-    const rand = parseInt(Math.random() * 5);
-    // const card = item.closest(".card");
-    const card = this.closest(".card");
-    card.style.backgroundColor = colorPallet[rand];
-    const para = card.querySelector("p");
-    let content = parseInt(para.textContent);
-    para.textContent = content + 1;
-    para.style.color = "white";
-}
-
-
-
-
+// function addStyleAction(){
+//     const rand = parseInt(Math.random() * 5);
+//     // const card = item.closest(".card");
+//     const card = this.closest(".card");
+//     card.style.backgroundColor = colorPallet[rand];
+//     const para = card.querySelector("p");
+//     let content = parseInt(para.textContent);
+//     para.textContent = content + 1;
+//     para.style.color = "white";
+// }
