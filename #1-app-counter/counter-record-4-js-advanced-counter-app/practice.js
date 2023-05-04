996cc7481b0af77d27bec1b20b6e18669a775fb1
MOSTAFAMAHFOUZ <emostafamahfouz@gmail.com>
2023-05-04 11:45:44 +0300

    advanced counter app

---
+++ #1-app-counter/counter-record-4-js-advanced-counter-app/practice.js
@@ -0,0 +1,24 @@
+const btns = document.querySelectorAll(".btn");
+const colorPallet = ["red","green","blue","orange","yellow"];
+
+// console.log(btns[0]);
+
+btns.forEach(function(item){
+    item.addEventListener("click",addStyleAction)
+})
+
+
+function addStyleAction(){
+    const rand = parseInt(Math.random() * 5);
+    // const card = item.closest(".card");
+    const card = this.closest(".card");
+    card.style.backgroundColor = colorPallet[rand];
+    const para = card.querySelector("p");
+    let content = parseInt(para.textContent);
+    para.textContent = content + 1;
+    para.style.color = "white";
+}
+
+
+
+
